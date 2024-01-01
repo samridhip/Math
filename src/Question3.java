f431b55c7754544e5fb1d2161a9a1d4bbecad150
samridhip <pandeysamridhi1@gmail.com>
2024-01-01 22:38:07 +0530

    practice

---
+++ src/Question3.java
@@ -0,0 +1,12 @@
+public class Question3 {
+    public static void main(String[] args) {
+        double no=34.777;
+        if((no%1)==0){
+            System.out.println("It's not double");
+        }
+        else {
+            System.out.println("It's a double");
+        }
+        }
+    }
+
