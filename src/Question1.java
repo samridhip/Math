60fdc2e51bd793831a874afada4902696291a6d6
samridhip <pandeysamridhi1@gmail.com>
2024-01-01 21:58:03 +0530

    practice

---
+++ src/Question1.java
@@ -0,0 +1,9 @@
+public class Question1 {
+    public static void main(String[] args) {
+        int theory_no=300;
+        int practical_no=50;
+        int total_no=500;
+        int percentage_marks=((theory_no+practical_no)*100/total_no);
+        System.out.println("percentage is "+percentage_marks);
+    }
+}
