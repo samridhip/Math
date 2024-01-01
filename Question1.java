93a92ab20e667115173bb5b35450efd29b9a19bd
samridhip <pandeysamridhi1@gmail.com>
2024-01-01 21:44:16 +0530

    practice

---
+++ Question1.java
@@ -0,0 +1,9 @@
+public class Question1 {
+    public static void main(String[] args) {
+        int theory_no=286;
+        int practical_no=36;
+        int total_no=500;
+        int percentage=((theory_no +practical_no) * 100)/total_no;
+        System.out.println("The percentage is"+percentage);
+    }
+}
---
+++ Question2.java
@@ -0,0 +1,11 @@
+import java.util;
+public class Question2 {
+    public static void main(String[] args) {
+        double value=12.56;
+        double fractical_value=value%1;
+        double integral_value=value-fractical_value;
+        System.out.println("original value" + value);
+        System.out.println("fractional value"+ fractions_value);
+        System.out.println("integral_value"+integral_value);
+    }
+}
